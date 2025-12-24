ee619e7252d0ea8c2d364a9471f5a3773ff06c58
roaayala <roaayala.ahm@gmail.com>
2025-12-24 22:27:41 +0800

    js variables and operators: doen

--- 014-js-variables-and-operators/main.js
+++ 014-js-variables-and-operators/main.js
@@ -17,3 +17,25 @@ console.log(phi);
 
 // Variables summary
 // we could re-assign the value in with "let", but couldnt do that if we use "const"
+
+// NUMBERS
+// 1
+console.log(23 + 97);
+// 2
+console.log(1 + 5 + 99 + 11 + 45 + 0 + 4);
+// 3
+console.log((4 + 6 + 9) / 77);
+// 4
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+// 5
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
