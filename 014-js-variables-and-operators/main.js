320dda42e7eb6176d3b0b6fdaeb130ac71e1c70f
roaayala <roaayala.ahm@gmail.com>
2025-12-24 22:20:41 +0800

    js variables and operators: intro to variables done

--- 014-js-variables-and-operators/main.js
+++ 014-js-variables-and-operators/main.js
@@ -1,1 +1,19 @@
 console.log('Hello');
+//
+
+// Variables
+let firstName = 'Ahmad';
+console.log(firstName);
+
+firstName = 'Budi';
+console.log(firstName);
+
+const lastName = 'Roaayala';
+console.log(lastName);
+
+const phi = 3.14;
+// phi = 10;
+console.log(phi);
+
+// Variables summary
+// we could re-assign the value in with "let", but couldnt do that if we use "const"
