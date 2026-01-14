863ba5a30740d74c97a6e918428c684f2d7ee67c
roaayala <roaayala.ahm@gmail.com>
2026-01-14 22:56:12 +0800

    020-training-2-array-loop: task 9 done

--- 020-training-2-array-loop/foundations/09_sumAll/sumAll.js
+++ 020-training-2-array-loop/foundations/09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+	let sum = 0;
 
+	if (
+		x < 0 ||
+		y < 0 ||
+		typeof x !== 'number' ||
+		typeof y !== 'number' ||
+		!Number.isInteger(x) ||
+		!Number.isInteger(y)
+	) {
+		return 'ERROR';
+	}
+
+	if (x < y) {
+		for (let i = x; i <= y; i++) {
+			sum += i;
+		}
+	}
+
+	if (x > y) {
+		for (let i = x; i >= y; i--) {
+			sum += i;
+		}
+	}
+
+	return sum;
 };
+console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
