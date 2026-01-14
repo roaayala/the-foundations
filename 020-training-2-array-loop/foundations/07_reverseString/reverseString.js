b902310aa43fbb9e608e289118587d63965b0625
roaayala <roaayala.ahm@gmail.com>
2026-01-14 21:24:37 +0800

    020-training-2-array-loop: task 7 done

--- 020-training-2-array-loop/foundations/07_reverseString/reverseString.js
+++ 020-training-2-array-loop/foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	const split = string.split('');
+	const reverse = split.reverse();
+	const join = reverse.join('');
+	return join;
 };
 
 // Do not edit below this line
