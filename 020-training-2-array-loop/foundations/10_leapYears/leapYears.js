a6a9be27ec74d20ece623428c645ff04bcc5acd8
roaayala <roaayala.ahm@gmail.com>
2026-01-15 21:17:19 +0800

    020 training 2 array loop: task 10 done

--- 020-training-2-array-loop/foundations/10_leapYears/leapYears.js
+++ 020-training-2-array-loop/foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+// For example:
+// 1996 year is a leap year because it is divisible by 4 and not by 100;
+// 1800 year is not a leap year because it is divisible by 4, but also by 100 (and not by 400);
+// 2000 year is a leap year because it is evenly divisible by 400.
 
+const leapYears = function (x) {
+	if (x % 4 === 0 && x % 100 !== 0) return true;
+
+	if (x % 4 === 0 && x % 400 === 0) return true;
+
+	if (x % 4 !== 0 || x % 100 === 0) return false;
 };
 
 // Do not edit below this line
