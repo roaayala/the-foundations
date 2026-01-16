587b1d4b30cbb3eeec3784349eb5800ee4eac765
roaayala <roaayala.ahm@gmail.com>
2026-01-16 20:17:25 +0800

    022-events: done

---
+++ 022-events/main.js
@@ -0,0 +1,34 @@
+// const button = document.querySelector('#button');
+
+// Event listener method 2
+// button.onclick = () => console.log(`Clicked`);
+
+// Evenet listener method 3
+// button.addEventListener('click', () => {
+// 	console.log(`Clicked!`);
+// });
+
+// function clicked() {
+// 	console.log(`Clicked`);
+// }
+// button.addEventListener('click', clicked);
+
+// callback
+// button.addEventListener('click', (e) => {
+// 	console.log(e.target);
+// });
+
+// button.addEventListener('click', (e) => {
+// 	e.target.style.backgroundColor = 'blue';
+// });
+
+// SELECT AND EVENT FEW TARGET
+// const buttons = document.querySelectorAll('button');
+
+// buttons.forEach((button) => {
+// 	button.addEventListener('click', () => {
+// 		console.log(button.id);
+// 	});
+// });
+
+// USEFUL EVENTS: click, dbclick, keydown and keyup
