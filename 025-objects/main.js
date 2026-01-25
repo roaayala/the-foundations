e3c02dc939f9fec744e0db3718817bbaa2946a49
roaayala <roaayala.ahm@gmail.com>
2026-01-25 21:11:19 +0800

    objects: task from object in javascript.info is done

--- 025-objects/main.js
+++ 025-objects/main.js
@@ -52,3 +52,60 @@
 
 // loop inside object using for in
 // if the key are using integer, than sorting descending automatically, if this behavior want to avoid then stringify the key
+
+// TASK 1
+const user = {};
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+console.log(user);
+
+// TASK 2
+function isEmpty(object) {
+	return Object.keys(object).length === 0;
+}
+
+const schedule = {};
+console.log(isEmpty(schedule));
+
+schedule['8:30'] = 'get up';
+console.log(isEmpty(schedule));
+
+// TASK 3
+const salaries = {
+	John: 100,
+	Ann: 160,
+	Pete: 130,
+};
+
+let totalSalaries = 0;
+
+for (const salary in salaries) {
+	if (Object.keys(salaries).length !== 0) {
+		totalSalaries += salaries[salary];
+	}
+}
+
+console.log(totalSalaries);
+
+// TASK 4
+// before the call
+function multiplyNumeric(object) {
+	for (const key in object) {
+		if (typeof object[key] === 'number') {
+			object[key] *= 2;
+		}
+	}
+}
+
+const menu = {
+	width: 200,
+	height: 300,
+	title: 'My menu',
+};
+console.log(menu);
+
+multiplyNumeric(menu);
+
+console.log(menu);
