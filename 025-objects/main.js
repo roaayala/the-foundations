6b4202a57a4ed7b5da6f7d789726b41bf177f800
roaayala <roaayala.ahm@gmail.com>
2026-01-25 20:35:15 +0800

    objects: literals and properties

--- 025-objects/main.js
+++ 025-objects/main.js
@@ -1,2 +1,17 @@
-const user = { name: 'roaayala', age: 27 };
-console.log(user, user.name, user.age);
+// LITERALS AND PROPERTIES
+// const user = { name: 'roaayala', age: 27 };
+// console.log(user);
+
+// user.isAdmin = true; // add new property
+// console.log(user);
+
+// delete user.age; // delete property
+// console.log(user);
+
+// user['likes birds'] = false; // multiword key must be inside quoute and select with square bracket
+// console.log(user['likes birds']);
+
+// const key = 'name';
+// console.log(user[key]); // handling dynamically object key using square bracket
+
+// COMPUTED PROPERTIES
