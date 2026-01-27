353262e73fb0d494cef7004b67251818b57fa32a
roaayala <roaayala.ahm@gmail.com>
2026-01-27 21:26:05 +0800

    object: training 3-13 done

--- 026-training-3/foundations/13_palindromes/palindromes.js
+++ 026-training-3/foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (x) {
+	const lowerCase = x.toLowerCase();
 
+	const splitAndTrim = lowerCase
+		.split('')
+		.filter((x) => x !== ' ')
+		.filter((x) => x !== '!')
+		.filter((x) => x !== '.')
+		.filter((x) => x !== ',');
+	const join = splitAndTrim.join('');
+
+	const reverse = splitAndTrim.reverse();
+	const joinReverse = reverse.join('');
+
+	return join === joinReverse ? true : false;
 };
-
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
