28c02cc142013b7a779012b9d8a9595b3e448e39
roaayala <roaayala.ahm@gmail.com>
2026-01-28 21:09:04 +0800

    object: training 3-14 done

--- 026-training-3/foundations/14_fibonacci/fibonacci.js
+++ 026-training-3/foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+	const arr = [0, 1];
+	let res = 0;
+	let target = 0;
 
+	if (x < 0) {
+		return 'OOPS';
+	}
+
+	if (x === 1 || x === '1') {
+		return 1;
+	}
+
+	for (let i = 2; i <= x; i++) {
+		res = arr[i - 2] + arr[i - 1];
+		arr.push(res);
+		target = arr[i];
+	}
+
+	return target;
 };
 
 // Do not edit below this line
