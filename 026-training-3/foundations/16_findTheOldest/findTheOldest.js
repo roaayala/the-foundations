3ae2a0e52ae7010f9226fa2837ba0c125f847500
roaayala <roaayala.ahm@gmail.com>
2026-01-28 21:33:38 +0800

    object: training 3-16 done

--- 026-training-3/foundations/16_findTheOldest/findTheOldest.js
+++ 026-training-3/foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (x) {
+	return x.reduce((oldest, current) => {
+		if (!current.yearOfDeath) {
+			current.yearOfDeath = new Date().getFullYear();
+		}
+		const age = current.yearOfDeath - current.yearOfBirth;
 
+		if (oldest.yearOfDeath - oldest.yearOfBirth > age) {
+			oldest = oldest;
+		} else {
+			oldest = { ...current };
+		}
+
+		return oldest;
+	}, {});
 };
 
 // Do not edit below this line
