894370e260a7c3124b4fb44cfd97696e0bef599f
roaayala <roaayala.ahm@gmail.com>
2026-01-28 21:15:42 +0800

    object: training 3-15 done

--- 026-training-3/foundations/15_getTheTitles/getTheTitles.js
+++ 026-training-3/foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+	return arr.reduce((titles, book) => {
+		titles.push(book.title);
+		return titles;
+	}, []);
 };
 
 // Do not edit below this line
